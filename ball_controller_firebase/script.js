b33f04f3fb4733ab6077268a061cbb1b606b1ab6
Alejandro <matamala@material.cl>
2017-09-10 18:03:46 -0400

    multiple users for 1 users

--- ball_controller_firebase/script.js
+++ ball_controller_firebase/script.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+ apiKey: "AIzaSyCVas_rM__4iheoLU1grsEfpc8zDx-XS8Q",
+ authDomain: "neat-vista-141916.firebaseapp.com",
+ databaseURL: "https://neat-vista-141916.firebaseio.com",
+ projectId: "neat-vista-141916",
+ storageBucket: "neat-vista-141916.appspot.com",
+ messagingSenderId: "155843876516"
+};
+firebase.initializeApp(config);
+
 var ref = firebase.database().ref('state');
 
 var left = document.getElementById("left");
